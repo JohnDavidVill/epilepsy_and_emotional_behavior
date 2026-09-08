5acd8a69af3a3c6d059175c0c91a7f8aeef11d94
John David Villarreal <johndavidvill02@gmail.com>
2026-09-08 16:42:11 -0500

    Added daemon reload

--- gpio-on.sh
+++ gpio-on.sh
@@ -3,6 +3,7 @@
 # Made by John David Villarreal 09/08/26
 # Ensure gpio-pulse.service and gpio-pulse.timer exist in /etc/systemd/system
 
+sudo systemctl daemon-reload
 sudo systemctl enable --now gpio-pulse.timer
 echo "gpio-pulse.timer started"
 sudo systemctl list-timers gpio-pulse.timer --no-pager

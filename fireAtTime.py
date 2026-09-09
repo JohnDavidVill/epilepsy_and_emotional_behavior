a5b5dffd42cca3b6c35a81ce296db9c2eb9c4fd5
John David Villarreal <johndavidvill02@gmail.com>
2026-09-09 11:27:16 -0500

    Mofified motor firing to pulses

--- fireAtTime.py
+++ fireAtTime.py
@@ -1,4 +1,4 @@
-# fireAtTime.py, created by John David Villarreal 09/08/26
+# fireAtTime.py, created by John David Villarreal 09/09/26
 # Control Motors at a specified interval
 
 import logging
@@ -12,8 +12,10 @@ GPIO.setup(26, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(6, GPIO.OUT)
 
-ON_DURATION = 30 # Duration in Seconds
-LOG_FILE = "./gpio_pulse.log"
+ON_PULSE = 0.5 # Time on per period in Seconds
+OFF_PULSE = 0.5 # Time off per period in seconds
+N_PERIODS = 20 # Number of periods
+LOG_FILE = "/home/epilepsy_emotion/Documents/programs/epilepsy_emotion/gpio_pulse.log"
 
 logging.basicConfig(
         filename=LOG_FILE,
@@ -23,23 +25,32 @@ logging.basicConfig(
 
 def main():
 
-    logging.info(f"Pulsing GPIOs for {ON_DURATION} seconds")
+    logging.info("Pulsing GPIO Pins")
     
     try:
-        # Turn on motors
-        GPIO.output(26, 1)
-        GPIO.output(16, 1)
-        GPIO.output(6, 1)
-        
-        # Keep on for duration
-        time.sleep(ON_DURATION)
+
+        for i in range(N_PERIODS):
+            
+            # Turn on Motors
+            GPIO.output(26, 1)
+            GPIO.output(16, 1)
+            GPIO.output(6, 1)
+            logging.info(f"On pulse {i+1} out of {N_PERIODS} complete")
+            time.sleep(ON_PULSE)
+            
+            # Turn off Motors
+            GPIO.output(26, 0)
+            GPIO.output(16, 0)
+            GPIO.output(6, 0)
+            logging.info(f"Off pulse {i+1} out of {N_PERIODS} complete")
+            time.sleep(OFF_PULSE)
 
     finally:
         GPIO.output(26, 0)
         GPIO.output(16, 0)
         GPIO.output(6, 0)
     
-    logging.info("Pulse complete")
+    logging.info(f"{N_PERIODS} of pulses complete")
 
 if __name__ == "__main__":
     main()
